cff321b741f8ce4014f2dd7f6e88d3752a4698ce
Jon Christie <jonpchristie@gmail.com>
2023-08-01 06:02:08 -0400

    playing with login popup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Jumbotron from './components/Jumbotron';
 import Navbar from './components/NavBar';
+import LoginPage from './components/LoginPage';
 
 function App() {
+  const [loginVisible, setLoginVisible] = useState(false);
+
+  // Create a function that toggles the LoginPage visibility
+  const toggleLogin = () => {
+    setLoginVisible(!loginVisible);
+  };
+
   return (
-    <div>
-      <Navbar />
-      <Jumbotron />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={toggleLogin}
+        >
+          Login
+        </button>
+        {loginVisible && <LoginPage />}
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
